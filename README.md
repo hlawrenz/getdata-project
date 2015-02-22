bf459965bb16fa088cb5a589bbbf8ac0fce9bf63
Hans Lawrenz <hrlawrenz@gmail.com>
2015-02-22 11:31:40 -0500

    add latest

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-getdata-project
-===============
+# getdata-project
+
+### Description
+
+The run-analysis.R script merges the test and training sets, extracts
+just the mean and standard deviation features for each measurment,
+applies descriptive names to the activity tag and labels the columns.
+It then outputs a summary data set containing the average of the
+selected data points for each activity.
+
+Descriptions of output data can be found in [CodeBook.md](CodeBook.md)
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,23 @@
 library(dplyr)
+
+# read in the names of the features
 feature_names <- read.table("UCI HAR Dataset/features.txt")    
+feature_names$keep <- grepl("mean\\Q()\\E", feature_names$V2) | grepl("std\\Q()\\E", feature_names$V2)
 
 # read in the test data and combine
 Xtest <- read.table("UCI HAR Dataset/test/X_test.txt", col.names=feature_names$V2)
+
+# only keep select columns
+Xtest <- Xtest[,feature_names$keep]
+
 ytest <- read.table("UCI HAR Dataset/test/y_test.txt", col.names=c("label"))
 Xtest$label <- ytest$label
 
 # read in the train data and combine
 Xtrain <- read.table("UCI HAR Dataset/train/X_train.txt", col.names=feature_names$V2)
+
+# only keep select columns
+Xtrain <- Xtrain[,feature_names$keep]
 ytrain <- read.table("UCI HAR Dataset/train/y_train.txt", col.names=c("label"))
 Xtrain$label <- ytrain$label
 
@@ -19,12 +29,14 @@ descriptive_act <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTI
 Xfull$label <- as.factor(Xfull$label)
 levels(Xfull$label) <- descriptive_act
 
+write.table(Xfull, file="Xfull_output.txt", row.name=FALSE)
+
 # only include columns relating to mean and standard deviation and the label
-Xclean <- Xfull[, c(grep("mean()", colnames(Xfull)), grep("std()", colnames(Xfull)), grep("label", colnames(Xfull)))]
+#Xclean <- Xfull[, c(grep("mean()", colnames(Xfull)), grep("std()", colnames(Xfull)), grep("label", colnames(Xfull)))]
 
 # summarise
-Xgrouped <- group_by(Xclean, label)
-Xsummary <- Xclean %>%  group_by(label) %>%  summarise_each(funs(mean))
+Xgrouped <- group_by(Xfull, label)
+Xsummary <- Xfull %>%  group_by(label) %>%  summarise_each(funs(mean))
 
 # output the results
 write.table(Xsummary, file="output.txt", row.name=FALSE)
